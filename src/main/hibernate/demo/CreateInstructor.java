4212f854b93127c1cfd05c484ccdb49be66678d8
Demotivirus <demotivirus@gmail.com>
2019-05-04 09:00:48 +0300

    Delete log4j, add CreateInstructor

---
+++ src/main/hibernate/demo/CreateInstructor.java
@@ -0,0 +1,53 @@
+package main.hibernate.demo;
+
+import main.hibernate.entity.Instructor;
+import main.hibernate.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class CreateInstructor {
+
+    public static void main(String[] args) {
+
+        //Create session factory
+        SessionFactory sessionFactory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        //Create session
+        Session session = sessionFactory.getCurrentSession();
+
+        try{
+
+            //Create the objects
+//            Instructor instructor = new Instructor("Chack", "Norris", "dontDisturb@mrNorris.com");
+//            InstructorDetail instructorDetail =
+//                    new InstructorDetail("hollywood@ismyland.com", "break the wall");
+
+            Instructor instructor = new Instructor("Bender", "Rodriguez", "cissMyMetal@shiny.ass");
+            InstructorDetail instructorDetail =
+                    new InstructorDetail("asino777", "Smoking cigars");
+
+            //Associate obj
+            instructor.setInstructorDetail(instructorDetail);
+
+            //Start transaction
+            session.beginTransaction();
+
+            //Save the instructor
+            //All it's save because CascadeType.ALL
+            System.out.println("Saving instructor: " + instructor);
+            session.save(instructor);
+
+            //Commit transaction
+            session.getTransaction().commit();
+            System.out.println("Done!");
+
+        } finally {
+            session.close();
+        }
+    }
+}
