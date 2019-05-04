443ff611c7f27b5667cc258bc7d1f03cd7c37814
Demotivirus <demotivirus@gmail.com>
2019-05-04 08:13:52 +0300

    Create repository

---
+++ src/main/jdbc/TestJdbc.java
@@ -0,0 +1,26 @@
+package main.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class TestJdbc {
+    public static void main(String[] args) {
+        String jdbcUrl = "jdbc:mysql://localhost:3306/hb-01-one-to-one-uni" +
+        "?verifyServerCertificate=false"+
+                "&useSSL=false"+
+                "&requireSSL=false"+
+                "&useLegacyDatetimeCode=false"+
+                "&amp"+
+                "&serverTimezone=UTC";
+
+        String login = "root";
+        String pass = "root";
+
+        try{
+            Connection connection = DriverManager.getConnection(jdbcUrl, login, pass);
+            System.out.println("Connection successful");
+        } catch (Exception ex){
+            ex.printStackTrace();
+        }
+    }
+}
